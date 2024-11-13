e616b97820e6159c445287d0d1e6cd9bbdda0c65
Valentino <valentino.kim@ui.ac.id>
2024-11-13 16:04:22 +0700

    finished tutorial 8

--- main/views.py
+++ main/views.py
@@ -1,25 +1,29 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse 
 from main.forms import MoodEntryForm
+from django.http import JsonResponse
 from main.models import MoodEntry
-from django.urls import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core import serializers
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+import datetime
+import json
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from django.utils.html import strip_tags
-import datetime
 
+# Create your views here.
 @login_required(login_url='/login')
 def show_main(request):
     context = {
+        'npm' : '2306275834',
         'name': request.user.username,
         'class': 'PBP F',
-        'npm': '2306275771',
-        'last_login': request.COOKIES['last_login'],
+        'last_login': request.COOKIES.get('last_login')
     }
 
     return render(request, "main.html", context)
@@ -36,13 +40,13 @@ def create_mood_entry(request):
     context = {'form': form}
     return render(request, "create_mood_entry.html", context)
 
-def show_xml(request):
-    data = MoodEntry.objects.filter(user=request.user)
-    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
-
 def show_json(request):
     data = MoodEntry.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml(request):
+    data = MoodEntry.objects.filter(user=request.user)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
 def show_xml_by_id(request, id):
     data = MoodEntry.objects.filter(pk=id)
@@ -54,20 +58,18 @@ def show_json_by_id(request, id):
 
 def register(request):
     form = UserCreationForm()
-
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
             messages.success(request, 'Your account has been successfully created!')
             return redirect('main:login')
-    context = {'form':form}
-    return render(request, 'register.html', context)
+    context = {'form': form}
+    return render(request, "register.html", context)
 
 def login_user(request):
    if request.method == 'POST':
       form = AuthenticationForm(data=request.POST)
-
       if form.is_valid():
         user = form.get_user()
         login(request, user)
@@ -75,7 +77,7 @@ def login_user(request):
         response.set_cookie('last_login', str(datetime.datetime.now()))
         return response
       else:
-          messages.error(request, "Invalid username or password. Please try again.")
+        messages.error(request, "Invalid username or password. Please try again.")
 
    else:
       form = AuthenticationForm(request)
@@ -127,3 +129,22 @@ def add_mood_entry_ajax(request):
     new_mood.save()
 
     return HttpResponse(b"CREATED", status=201)
+
+@csrf_exempt
+def create_mood_flutter(request):
+    if request.method == 'POST':
+
+        data = json.loads(request.body)
+        new_mood = MoodEntry.objects.create(
+            user=request.user,
+            mood=data["mood"],
+            mood_intensity=int(data["mood_intensity"]),
+            feelings=data["feelings"]
+        )
+
+        new_mood.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
+    
